ea85e4a1ef1b93096a21ba7e2ac87550c1851ea0
Ridhima <54638081+RidhimaKohli@users.noreply.github.com>
2021-10-06 15:26:53 +0530

    gui imp

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,7 +6,7 @@ import subprocess
 
 
 window=Tk()
-window.geometry("500x500")
+window.geometry("400x400")
 
 def display():
     subprocess.call("statCode.sh",shell=True)
@@ -17,12 +17,12 @@ def display():
     f.close()
 
 window.title("This is window")
-window.configure(background="#787A91")
-head = Label(window,text="Welcome to GUI for Linux !", font=("Arial", 20),bg="#1C0C5B",fg="white",justify=CENTER)
+window.configure(background="black")
+head = Label(window,text="Welcome to GUI for Linux !", font=("Arial", 20),bg="black",fg="white",justify=CENTER)
 head.grid(row=1,column=0)
-button =Button(window,text="Get memory details",width=25,height=5,command=display,bg="#0F044C",fg="white").grid(row=4,column=0)
+button =Button(window,text="Get memory details",width=25,height=5,command=display,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=4,column=0,pady=30)
 output=Text(window,width=75,height=6,wrap=WORD,background="white", font=(None, 10))
-output.grid(row=7,column=0,columnspan=2)
+output.grid(row=7,column=0,columnspan=2,pady=40)
 
 
 
--- GUI/statCode.sh
+++ GUI/statCode.sh
@@ -5,3 +5,5 @@ filename="out.txt"
 echo $mem>$filename
 st=$(grep MemFree /proc/meminfo)
 echo $st>>$filename
+st2=$(grep MemAvailable /proc/meminfo)
+echo $st2>>$filename
