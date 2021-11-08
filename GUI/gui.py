623a1a81d5dde47254cb269da2ff037684617ae4
Ridhima <54638081+RidhimaKohli@users.noreply.github.com>
2021-11-08 23:23:53 +0530

    details addded

--- GUI/gui.py
+++ GUI/gui.py
@@ -11,7 +11,15 @@ window.geometry("400x400")
 def display():
     subprocess.call("statCode.sh",shell=True)
     output.delete(0.0,END)
-    f = open("out.txt", "r")
+    f = open("memory.txt", "r")
+    data = f.read()
+    output.insert(END,data)
+    f.close()
+
+def display2():
+    subprocess.call("statCode.sh",shell=True)
+    output.delete(0.0,END)
+    f = open("cpu.txt", "r")
     data = f.read()
     output.insert(END,data)
     f.close()
@@ -20,9 +28,12 @@ window.title("This is window")
 window.configure(background="black")
 head = Label(window,text="Welcome to GUI for Linux !", font=("Arial", 20),bg="black",fg="white",justify=CENTER)
 head.grid(row=1,column=0)
-button =Button(window,text="Get memory details",width=25,height=5,command=display,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=4,column=0,pady=30)
+#mem
+button =Button(window,text="Get memory details",width=10,height=2,command=display,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=4,column=0,pady=30)
 output=Text(window,width=75,height=6,wrap=WORD,background="white", font=(None, 10))
 output.grid(row=7,column=0,columnspan=2,pady=40)
+#cpu
+button =Button(window,text="Get CPU details",width=10,height=2,command=display2,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=8,column=0,pady=30)
 
 
 
--- GUI/statCode.sh
+++ GUI/statCode.sh
@@ -1,9 +1,17 @@
-
 #!/bin/bash
-mem=$(grep MemTotal /proc/meminfo)
-filename="out.txt"
-echo $mem>$filename
+mem=$(grep MemTotal /proc/meminfo) 
+memory_file="memory.txt"
+echo $mem>$memory_file
 st=$(grep MemFree /proc/meminfo)
-echo $st>>$filename
+echo $st>>$memory_file
 st2=$(grep MemAvailable /proc/meminfo)
-echo $st2>>$filename
+echo $st2>>$memory_file
+proc_count=$(grep -c processor /proc/cpuinfo)
+p="Number of processor : "
+cpu_file="cpu.txt"
+p+=$proc_count
+echo $p>$cpu_file
+model_name=$(grep "model name" /proc/cpuinfo | head -1 | cut -d: -f2 | sed 's/^[ \t]*//;s/[ \t]*$//')
+m="Model : "
+m+=$model_name
+echo $m>>$cpu_file
