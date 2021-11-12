3ef5ac0779427f51fa5a856fd6619f6d62759df1
Chandradeep07 <suvarnakantichandradeep@gmail.com>
2021-11-13 01:48:49 +0530

    live graph done

--- GUI/graph.py
+++ GUI/graph.py
@@ -1,8 +1,13 @@
+from tkinter import font
+from matplotlib import colors
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 import psutil
 import collections
+plt.rcParams['text.color'] = 'white'
+# plt.title('Live Graph')
+
 
 def my_function(i):
     cpu_usage_array.popleft()
@@ -10,19 +15,21 @@ def my_function(i):
 
     ram_usage_array.popleft()
     ram_usage_array.append(psutil.virtual_memory().percent)
-    
+
     cpu_subplot.cla()
     ram_subplot.cla()
 
-    cpu_subplot.plot(cpu_usage_array, c='#008514')
-    cpu_subplot.scatter(len(cpu_usage_array)-1, cpu_usage_array[-1], c='#05fc2a')
-    cpu_subplot.text(len(cpu_usage_array)-1, cpu_usage_array[-1]+2, "{}%".format(cpu_usage_array[-1]))
+    cpu_subplot.plot(cpu_usage_array, c='#05fc2a')
+    cpu_subplot.scatter(len(cpu_usage_array)-1,
+                        cpu_usage_array[-1], c='#05fc2a')
+    cpu_subplot.text(len(cpu_usage_array)-1,
+                     cpu_usage_array[-1]+2, "{}%".format(cpu_usage_array[-1]))
 
-    cpu_subplot.set_xticks(np.arange(-1,10,2))
-    cpu_subplot.set_xticklabels(np.arange(10,-1,-2))
-    cpu_subplot.set_xlabel('Time (in seconds)')
+    cpu_subplot.set_xticks(np.arange(-1, 10, 2))
+    cpu_subplot.set_xticklabels(np.arange(10, -1, -2))
+    cpu_subplot.set_xlabel('Time (in seconds)').set_color('white')
     cpu_subplot.set_title('CPU Usage\n')
-    cpu_subplot.set_ylim(0,100)
+    cpu_subplot.set_ylim(0, 100)
 
     # remove spines
     cpu_subplot.spines['left'].set_visible(False)
@@ -33,15 +40,18 @@ def my_function(i):
     cpu_subplot.set_axisbelow(True)
     cpu_subplot.yaxis.grid(linestyle='dashed', alpha=0.8)
 
-    ram_subplot.plot(ram_usage_array, c='#990227')
-    ram_subplot.scatter(len(ram_usage_array)-1, ram_usage_array[-1], c='#990227')
-    ram_subplot.text(len(ram_usage_array)-1, ram_usage_array[-1]+2, "{}%".format(ram_usage_array[-1]))
-
-    ram_subplot.set_xticks(np.arange(-1,10,2))
-    ram_subplot.set_xticklabels(np.arange(10,-1,-2))
-    ram_subplot.set_xlabel('Time (in seconds)')
+    ram_subplot.plot(ram_usage_array, c='#EF1B1B')
+    ram_subplot.scatter(len(ram_usage_array)-1,
+                        ram_usage_array[-1], c='#EF1B1B')
+    ram_subplot.text(len(ram_usage_array)-1,
+                     ram_usage_array[-1]+2, "{}%".format(ram_usage_array[-1]))
+    # # change color of subplot text
+    # ram_subplot.texts[0].set_color('#EF1B1B')
+    ram_subplot.set_xticks(np.arange(-1, 10, 2))
+    ram_subplot.set_xticklabels(np.arange(10, -1, -2))
+    ram_subplot.set_xlabel('Time (in seconds)').set_color('white')
     ram_subplot.set_title('RAM Usage\n')
-    ram_subplot.set_ylim(0,100)
+    ram_subplot.set_ylim(0, 100)
     # remove spines
     ram_subplot.spines['left'].set_visible(False)
     ram_subplot.spines['right'].set_visible(False)
@@ -51,17 +61,21 @@ def my_function(i):
     ram_subplot.set_axisbelow(True)
     ram_subplot.yaxis.grid(linestyle='dashed', alpha=0.8)
 
+
 cpu_usage_array = collections.deque(np.zeros(10))
 ram_usage_array = collections.deque(np.zeros(10))
 
-fig = plt.figure(figsize=(10,6), facecolor='#DEDEDE')
+fig = plt.figure(figsize=(10, 6), facecolor='#434343', num='LIVE GRAPH')
 
 cpu_subplot = plt.subplot(122)
 ram_subplot = plt.subplot(121)
 
-cpu_subplot.set_facecolor('#DEDEDE')
-ram_subplot.set_facecolor('#DEDEDE')
-
+cpu_subplot.set_facecolor('#434343')
+ram_subplot.set_facecolor('#434343')
+cpu_subplot.tick_params(axis='x', colors='white')
+cpu_subplot.tick_params(axis='y', colors='white')
+ram_subplot.tick_params(axis='x', colors='white')
+ram_subplot.tick_params(axis='y', colors='white')
 animation = FuncAnimation(plt.gcf(), my_function, interval=1000)
 
 plt.show()
--- GUI/gui.py
+++ GUI/gui.py
@@ -1,46 +1,54 @@
 from tkinter import *
 import subprocess
+from tkinter import ttk
+from ttkthemes import ThemedStyle
+from ttkthemes import themed_style
 
 
+window = Tk()
+window.geometry('600x250')
 
-
-
-window=Tk()
-window.geometry("400x400")
 
 def display():
-    subprocess.call("statCode.sh",shell=True)
-    output.delete(0.0,END)
+    subprocess.call("statCode.sh", shell=True)
+    output.delete(0.0, END)
     f = open("memory.txt", "r")
     data = f.read()
-    output.insert(END,data)
+    output.insert(END, data)
     f.close()
+
 
 def display2():
-    subprocess.call("statCode.sh",shell=True)
-    output.delete(0.0,END)
+    subprocess.call("statCode.sh", shell=True)
+    output.delete(0.0, END)
     f = open("cpu.txt", "r")
     data = f.read()
-    output.insert(END,data)
+    output.insert(END, data)
     f.close()
 
+
 def display3():
-    subprocess.call("python graph.py",shell=True)
+    subprocess.call("python graph.py", shell=True)
 
 
-window.title("This is window")
-window.configure(background="black")
-head = Label(window,text="Welcome to GUI for Linux !", font=("Arial", 20),bg="black",fg="white",justify=CENTER)
-head.grid(row=1,column=0)
-#mem
-button =Button(window,text="Get memory details",width=10,height=2,command=display,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=4,column=0,pady=30)
-output=Text(window,width=75,height=6,wrap=WORD,background="white", font=(None, 10))
-output.grid(row=7,column=0,columnspan=2,pady=40)
-#cpu
-button =Button(window,text="Get CPU details",width=10,height=2,command=display2,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=8,column=0,pady=30)
+window.title("System Stats")
 
-button =Button(window,text="Get live graph",width=10,height=2,command=display3,font=("Arial", 15),bg="#A2D2FF",fg="black",activebackground="#90AACB", activeforeground="#fff").grid(row=16,column=0,pady=30)
+style = ThemedStyle(window)
+style.theme_use("equilux")
+window.configure(bg=style.lookup('TLabel', 'background'))
 
-
-
+# mem
+button1 = ttk.Button(window, text='Memory',
+                     width=10, command=display)
+button1.grid(row=1, column=0, pady=1)
+# cpu
+button2 = ttk.Button(window, text='Cpu',
+                     width=10, command=display2)
+button2.grid(row=3, column=0, pady=1)
+button3 = ttk.Button(window, text='Graph',
+                     width=10, command=display3)
+button3.grid(row=5, column=0, pady=1)
+output = Text(window, width=75, height=6, wrap=WORD,
+              font=(None, 10), bg='#434343', fg='#B4B2B2')
+output.grid(row=7, column=0, columnspan=1, pady=10, padx=30)
 window.mainloop()
